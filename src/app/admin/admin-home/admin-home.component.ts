3efe49344f7f712de3130ed4edfe62b450dc8fbc
lakshithamihiran <lakshithajayaweera@gmail.com>
2018-11-02 14:48:14 +0530

    add new files

--- src/app/admin/admin-home/admin-home.component.ts
+++ src/app/admin/admin-home/admin-home.component.ts
@@ -1,13 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+
 
 interface item {
-    name: string,
-    age: number,
-    email: string
+    id: number,
+
 }
 @Component({
     selector: 'app-admin-home',
@@ -19,43 +15,13 @@ export class AdminHomeComponent implements OnInit {
     firststeps: boolean = true;
     secondsteps: boolean = true;
     title: string;
-    items: Observable<item[]>;
+    items: any = [];
 
     constructor() {
     }
 
-    ngOnInit() {this.items = Observable.of([
-        {
-            "id": 1,
-            "name": "Laura",
-            "email": "lbutler0@latimes.com",
-            "age": 47
-        },
-        {
-            "id": 2,
-            "name": "Walter",
-            "email": "wkelley1@goodreads.com",
-            "age": 37
-        },
-        {
-            "id": 3,
-            "name": "Walter",
-            "email": "wgutierrez2@smugmug.com",
-            "age": 49
-        },
-        {
-            "id": 4,
-            "name": "Jesse",
-            "email": "jarnold3@com.com",
-            "age": 47
-        },
-        {
-            "id": 5,
-            "name": "Irene",
-            "email": "iduncan4@oakley.com",
-            "age": 33
-        }
-    ]);
+    ngOnInit() {
+        this.items.push('text_area' + (this.items.length + 1));
 
     }
 
@@ -64,6 +30,8 @@ export class AdminHomeComponent implements OnInit {
             this.firststeps = false;
             this.secondsteps = true;
         }
+        this.items.push('text_area' + (this.items.length + 1));
+
     }
 
 }
